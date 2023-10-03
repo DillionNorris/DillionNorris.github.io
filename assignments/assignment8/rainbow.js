ddad7a22cce5315c61f65592f4ce7bd495532d3d
cyber <dillionanorris@gmail.com>
2023-10-03 12:43:06 -0400

    added format for new assignments and worked on assignment 8' git push

---
+++ assignments/assignment8/rainbow.js
@@ -0,0 +1,39 @@
+
+
+
+const displayRainbow =()=>{
+
+    const num = 2000    // number of milliseconds 
+    //disp red
+    setTimeout(red, num);
+    //display orange
+    setTimeout(orange, num);
+    //display yellow
+    setTimeout(yellow, num);
+    //display green
+    setTimeout(green, num);
+    //display blue
+    setTimeout(blue, num);
+    //display purp
+    setTimeout(purple, num);
+    //display pot
+    setTimeout(pot, num);
+}
+
+const red = ()=>{document.getElementById("red").style.visable;}
+const orange = ()=>{document.getElementById("orange").style.visable;}
+const yellow = ()=>{document.getElementById("yellow").style.visable;}
+const green = ()=>{document.getElementById("green").style.visable;}
+const blue = ()=>{document.getElementById("blue").style.visable;}
+const purple = ()=>{document.getElementById("purple").style.visable;}
+const pot = ()=>{document.getElementById("pot").style.visable;}
+    
+
+    
+
+
+
+
+window.onload =()=>{
+document.getElementById("rainbow-button").onclick = displayRainbow();
+}
