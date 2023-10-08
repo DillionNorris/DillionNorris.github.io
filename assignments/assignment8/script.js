103ef0510af222f1d64ff932e8c018a759c86f16
cyber <dillionanorris@gmail.com>
2023-10-08 13:12:59 -0400

    added webside project

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -1,16 +1,24 @@
+
+
 // vars
 let quotes = [1, 2, 3, 4, 5];
 let index = 0;
-
+const elemnt = document.getElementById(".quote");
 //functions
+
 const displayQuotes = () => {
-  if (index == 4) index = 0;
-  document.getElementById("quote").innerHTML = quotes[index];
+  if (index == 4){
+   index = 0;
+  }
+  console.log("worked");
+  elemnt.innerHTML ="123";
   index++;
+  
 };
 
 //main
-displayQuotes();
+window.onload = displayQuotes();
+
 
 /*
 while(true){
